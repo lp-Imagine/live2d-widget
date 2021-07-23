b416a2fcca4683e8ec681a56db142894f66d5637
lp-Imagine <55044461+lp-Imagine@users.noreply.github.com>
2021-07-23 17:01:24 +0800

    Update autoload.js

--- autoload.js
+++ autoload.js
@@ -1,7 +1,7 @@
 // 注意：live2d_path 参数应使用绝对路径
 // const live2d_path = "https://cdn.jsdelivr.net/gh/stevenjoezhang/live2d-widget@latest/";
 //const live2d_path = "/live2d-widget/";
-const live2d_path = "https://cdn.jsdelivr.net/gh/lp-Imagine/live2d-widget@2.0/";
+const live2d_path = "https://cdn.jsdelivr.net/gh/lp-Imagine/live2d-widget@3.0/";
 
 // 封装异步加载资源的方法
 function loadExternalResource(url, type) {
@@ -35,7 +35,8 @@ if (screen.width >= 768) {
 		initWidget({
 			waifuPath: live2d_path + "waifu-tips.json",
 			//apiPath: "https://live2d.fghrsh.net/api/",
-			cdnPath: "https://cdn.jsdelivr.net/gh/fghrsh/live2d_api/"
+ 			//cdnPath: "https://cdn.jsdelivr.net/gh/fghrsh/live2d_api/"
+			cdnPath: "https://unpkg.zhimg.com/akilar-live2dapi/"
 		});
 	});
 }
